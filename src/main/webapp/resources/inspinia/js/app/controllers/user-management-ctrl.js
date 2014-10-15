d86fb5ba7e4207625bbd337decec8d84e8fa86a2
cadizlester <cadizlester@gmail.com>
2014-10-15 09:52:59 +0800

    rearrange structures

--- src/main/webapp/resources/inspinia/js/app/controllers/user-management-ctrl.js
+++
@@ -1,6 +0,0 @@
-var userManagementCtrls = angular.module('userManagementCtrl', ['ngResource', 'ngSanitize']);
-
-userManagementCtrls.controller('userListCtrl', ['$scope', '$http', 'userFactory',
-    function($scope,  $http, userFactory) {
-
-}]);
--- src/main/webapp/resources/inspinia/js/app/factories/user-factory.js
+++
@@ -1,8 +0,0 @@
-var userManagementFactory = angular.module('userManagementFactory', []);
-
-userManagementFactory.factory('userFactory', ['$http', function($http) {
-    this.getUsers = function () {
-        return $http.get('/user/list');
-    };
-    return this;
-}]);
